9e04211e23692a5f2338fe31fcbc2162e039b6f7
petersudai <psudai@gmail.com>
2024-05-03 10:39:40 -0700

    Api App update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Create Flask app and app_views""""
+"""Create Flask app and app_views"""
 
 from api.v1.views import app_views
 from flask import jsonify
 
 @app_views.route('/status', methods=['GET'])
 def api_status():
+    """Jsonify"""
     return jsonify({"status": "OK"})
