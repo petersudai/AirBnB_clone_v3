ea86acac0fddc11a15f21d685aa6eee96603a3c2
petersudai <psudai@gmail.com>
2024-05-03 13:41:34 -0700

    update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,12 +12,6 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """Close storage"""
-    storage.close()
-
-
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
