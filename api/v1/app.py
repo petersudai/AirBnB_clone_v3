f4beb170c2702e489a21c65323945882a5eef53d
petersudai <psudai@gmail.com>
2024-05-03 14:24:29 -0700

    handling 404 responses in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Create Flask App
 """
 from os import getenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -14,7 +14,8 @@ app.register_blueprint(app_views)
 
 def not_found(error):
     """Handles 404 error using JSON formatted responses"""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
